2b2162571a946d0fbd1dc51dc622f1bceca926cc
geraLv <gerardoluisvega@gmail.com>
2026-07-08 11:42:46 -0300

    fix en el registro de geolocalizacion de clientes

--- src/main/java/com/sistemagas/pedidos/dto/request/ClienteRequest.java
+++ src/main/java/com/sistemagas/pedidos/dto/request/ClienteRequest.java
@@ -3,6 +3,8 @@ package com.sistemagas.pedidos.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class ClienteRequest {
     @NotBlank(message = "El nombre es obligatorio")
@@ -12,4 +14,8 @@ public class ClienteRequest {
 
     @NotBlank(message = "La direccion es obligatoria")
     private String direccion;
+
+    private BigDecimal latitud;
+    
+    private BigDecimal longitud;
 }
