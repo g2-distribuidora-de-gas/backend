ed516917ffa3ebfe63f57f28349f96927754dce4
geraLv <gerardoluisvega@gmail.com>
2026-07-16 11:24:28 -0300

    fix en la replicacion de rutas

--- src/main/java/com/sistemagas/pedidos/dto/request/SincronizacionParadasRequest.java
+++ src/main/java/com/sistemagas/pedidos/dto/request/SincronizacionParadasRequest.java
@@ -3,12 +3,16 @@ package com.sistemagas.pedidos.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "DTO para sincronizar un lote de paradas offline")
 public class SincronizacionParadasRequest {
 
--- src/main/java/com/sistemagas/pedidos/dto/request/SincronizacionRutasRequest.java
+++ src/main/java/com/sistemagas/pedidos/dto/request/SincronizacionRutasRequest.java
@@ -3,13 +3,17 @@ package com.sistemagas.pedidos.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "DTO para sincronizar un lote de cambios de estado de rutas offline")
 public class SincronizacionRutasRequest {
 
